368e02abb0cad88fdd1322d4407c3c26a3f00db0
techc0der <sandipsuthar068@gmail.com>
2024-08-19 13:41:42 +0530

    database is created

--- database.js
+++ database.js
@@ -1,3 +1,20 @@
+const mongoose = require('mongoose');
 require('dotenv').config;
-
 const URL=process.env.DB_URL;
+
+mongoose.connect(URL);
+
+const db = mongoose.connection;
+
+db.on('connected',()=>{
+    console.log('Database is Connected with server');
+})
+db.on('disconnected',()=>{
+    console.log('Database is Disconnected with server');
+})
+db.on('error',()=>{
+    console.log('Error in Database');
+    ;
+})
+
+module.export = db;
