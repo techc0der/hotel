f32fc2601e60af2e2c85c70f3da9348e6b0209cc
techc0der <sandipsuthar068@gmail.com>
2024-08-20 22:21:24 +0530

    add unique feature in name schema

--- menu/foodSchema.js
+++ menu/foodSchema.js
@@ -6,7 +6,8 @@ const foodItemSchema = new foodSchema({
     name: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
+      unique:true
     },
     description: {
       type: String,
